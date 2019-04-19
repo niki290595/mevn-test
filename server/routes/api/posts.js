2a551c5a0a767fdfb8051eda41218910134cd95e
nikitinilya95 <nikitin_ilya@outlook.com>
2019-04-19 06:41:42 +0500

    Add api operations

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -5,13 +5,38 @@ const router = express.Router();
 
 //get post
 router.route('/posts/')
-    .get((req, res) => {
-        res.send('hello');
+    .get(async (req, res) => {
+        const posts = await loadPostCollection();
+        res.send(await posts.find({}).toArray());
+    })
+    .post(async  (req, res) => {
+        const posts = await loadPostCollection();
+        await posts.insertOne({
+            text: req.body.text,
+            createdAt: new Date()
+        });
+        res.status(201).send();
+    });
+router.route('/posts/:id')
+    //.get()
+    //.put()
+    .delete(async (req, res) => {
+        const posts = await loadPostCollection();
+        await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+        res.status(200).send();
     });
 
 //add post
 
 //delete post
 
+async function loadPostCollection() {
+    const client = await mongodb.MongoClient.connect(
+        'mongodb+srv://niki:OREdqsFrle0FKgRP@cluster0-kfimm.mongodb.net/test?retryWrites=true', {
+            useNewUrlParser: true
+        });
+
+    return client.db('vue_express').collection('posts')
+}
 
 module.exports = router;
