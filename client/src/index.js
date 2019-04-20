9ff7921cac78bcf1f1615df7ff0002fa19f0f1ca
nikitinilya95 <nikitin_ilya@outlook.com>
2019-04-20 06:06:52 +0500

    Add routing

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,12 @@ import './scss/main.scss'
 
 window.Vue = require('vue');
 import store from './store/index';
+import router from './router';
 
-Vue.component('example-component', require('./components/Example.vue').default);
+//Vue.component('example-component', require('./components/Example.vue').default);
 
 const app = new Vue({
     store,
+    router,
     el: '#app'
 });
