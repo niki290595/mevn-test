b8793ce8069f800118eabdd72005f3f21a514932
nikitinilya95 <nikitin_ilya@outlook.com>
2019-05-03 13:35:31 +0500

    Create sign up form

--- client/src/components/views/Signup.vue
+++ client/src/components/views/Signup.vue
@@ -1,141 +1,48 @@
 <template>
-    <section>
-        <h1>Signup</h1>
-        <div v-if="signingUp">
-            <img src="/assets/img/pacman_loading.svg" />
-        </div>
-        <div v-if="errorMessage" class="alert alert-danger" role="alert">
-            {{ errorMessage }}
-        </div>
-        <form v-if="!signingUp" @submit.prevent="signup">
-            <div class="form-group">
-                <label for="username">Username</label>
-                <input
-                        v-model="user.username"
-                        type="text"
-                        class="form-control"
-                        id="username"
-                        aria-describedby="usernameHelp"
-                        placeholder="Enter a username" required>
-                <h5 id="usernameHelp" class="form-text text-muted">
-                    Username must be longer than 2 characters and shorter than 30.
-                    Username can only contain alphanumeric characters and under_scores.
-                </h5>
-            </div>
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="password">Password</label>
-                    <input
-                            v-model="user.password"
-                            type="password"
-                            class="form-control"
-                            id="password"
-                            aria-describedby="passwordHelp"
-                            placeholder="Password" required>
-                    <h5 id="passwordHelp" class="form-text text-muted">
-                        Password must be 10 or more characters long.
-                    </h5>
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="confirmPassword">Confirm Password</label>
-                    <input
-                            v-model="user.confirmPassword"
-                            type="password"
-                            class="form-control"
-                            id="confirmPassword"
-                            aria-describedby="confirmPasswordHelp"
-                            placeholder="Password" required>
-                    <h5 id="confirmPasswordHelp" class="form-text text-muted">
-                        Please confirm your password.
-                    </h5>
-                </div>
-            </div>
-            <button type="submit" class="btn btn-primary">Signup</button>
-        </form>
-    </section>
+    <v-form class="ma-3">
+        <v-container>
+            <v-layout column>
+                <v-flex>
+                    <h5 class="headline">Регистрация</h5>
+                </v-flex>
+
+                <v-flex>
+                    <v-text-field label="Email"
+                                  prepend-icon="person_outline">
+                    </v-text-field>
+                </v-flex>
+
+                <v-flex>
+                    <v-text-field label="Пароль"
+                                  type="password"
+                                  prepend-icon="lock_outline">
+                    </v-text-field>
+                </v-flex>
+
+                <v-flex>
+                    <v-text-field label="Повторите пароль"
+                                  type="password"
+                                  prepend-icon="lock_outline">
+                    </v-text-field>
+                </v-flex>
+
+                <v-flex>
+                    <v-btn class="primary-gradient"
+                           color round block>
+                        Регистрация
+                    </v-btn>
+                </v-flex>
+
+                <v-flex class="body-1">
+                    <a>Уже есть аккаунт? Войти</a>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </v-form>
 </template>
 
 <script>
-    import Joi from 'joi';
-    const SIGNUP_URL = 'http://localhost:5000/auth/signup';
-    const schema = Joi.object().keys({
-        username: Joi.string().regex(/(^[a-zA-Z0-9_]+$)/).min(2).max(30)
-            .required(),
-        password: Joi.string().trim().min(10).required(),
-        confirmPassword: Joi.string().trim().min(10).required(),
-    });
-    export default {
-        data: () => ({
-            signingUp: false,
-            errorMessage: '',
-            user: {
-                username: '',
-                password: '',
-                confirmPassword: '',
-            },
-        }),
-        watch: {
-            user: {
-                handler() {
-                    this.errorMessage = '';
-                },
-                deep: true,
-            },
-        },
-        methods: {
-            signup() {
-                this.errorMessage = '';
-                if (this.validUser()) {
-                    const body = {
-                        username: this.user.username,
-                        password: this.user.password,
-                    };
-                    this.signingUp = true;
-                    fetch(SIGNUP_URL, {
-                        method: 'POST',
-                        body: JSON.stringify(body),
-                        headers: {
-                            'content-type': 'application/json',
-                        },
-                    }).then((response) => {
-                        if (response.ok) {
-                            return response.json();
-                        }
-                        return response.json().then((error) => {
-                            throw new Error(error.message);
-                        });
-                    }).then((result) => {
-                        localStorage.token = result.token;
-                        setTimeout(() => {
-                            this.signingUp = false;
-                            this.$router.push('/dashboard');
-                        }, 1000);
-                    }).catch((error) => {
-                        setTimeout(() => {
-                            this.signingUp = false;
-                            this.errorMessage = error.message;
-                        }, 1000);
-                    });
-                }
-            },
-            validUser() {
-                if (this.user.password !== this.user.confirmPassword) {
-                    this.errorMessage = 'Passwords must match. 🙈';
-                    return false;
-                }
-                const result = Joi.validate(this.user, schema);
-                if (result.error === null) {
-                    return true;
-                }
-                if (result.error.message.includes('username')) {
-                    this.errorMessage = 'Username is invalid. 😭';
-                } else {
-                    this.errorMessage = 'Password is invalid. 🙈';
-                }
-                return false;
-            },
-        },
-    };
+    export default {}
 </script>
 
 <style>
