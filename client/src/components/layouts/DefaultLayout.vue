d66a458d09b0d57a0a212a8ddcf5822bb5eb8ddf
nikitinilya95 <nikitin_ilya@outlook.com>
2019-05-16 06:04:42 +0500

    Add header and footer, logout action

--- client/src/components/layouts/DefaultLayout.vue
+++ client/src/components/layouts/DefaultLayout.vue
@@ -1,14 +1,52 @@
 <template>
     <div>
-        DEFAULT
-        <div class="container">
-            <slot/>
-        </div>
+        <v-toolbar class="primary-gradient" app>
+            <v-toolbar-title>
+                <router-link to="/">Заголовок</router-link>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+
+            <v-toolbar-items>
+                <v-btn v-if="$store.getters.isLoggedIn" flat
+                       @click="logout()">
+                    Выйти
+                </v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+
+        <v-content>
+            <v-container class="pa-0" fluid>
+                <v-layout row wrap align-center>
+                    <v-flex>
+                        <slot/>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-content>
+
+        <v-footer app>
+            <v-layout>
+                <v-flex class="primary-gradient"
+                        py-3
+                        text-xs-center>
+                    &copy; {{ new Date().getFullYear() }}
+                </v-flex>
+            </v-layout>
+        </v-footer>
     </div>
 </template>
 
 <script>
-  export default {}
+  export default {
+    data: () => ({
+    }),
+    methods: {
+      logout() {
+        this.$store.commit('logout');
+        this.$router.push('/login');
+      }
+    }
+  }
 </script>
 
 <style>
